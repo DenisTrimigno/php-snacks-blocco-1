81ef6d1965eab69af1d9e962075fbaa19bc108e6
MyUsernameDenisTrimignogit config --global user.email my@email.comdenis@trimigno.it <denis@trimigno.it>
2023-07-06 01:23:31 +0200

    snack 2 e 3

--- index.php
+++ index.php
@@ -10,25 +10,29 @@
        ["squadra_casa" => "Dinamo Sassari", "squadra_ospite" => "Reggiana", "punti_casa" => 65, "punti_ospite" => 75]
    ];
    
+   
 
    //Snack 2
    //Con un form passare come parametri GET name, mail e age e verificare 
    //(cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, 
    // che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
    
+       $name = $_GET['nome'];
+       $mail = $_GET['e-mail'];
+       $age = $_GET['età'];
    
    if (isset($_GET['name']) && isset($_GET['mail']) && isset($_GET['age'])) {
-       $name = $_GET['name'];
-       $mail = $_GET['mail'];
-       $age = $_GET['age'];
+       
    
        // Verifica che name sia più lungo di 3 caratteri
        if (strlen($name) > 3) {
            // Verifica che mail contenga un punto e una chiocciola
            if (strpos($mail, '.') !== false && strpos($mail, '@') !== false) {
                // Verifica che age sia un numero
+
                if (is_numeric($age)) {
                    echo "Accesso riuscito";
+                   
                } else {
                    echo "Accesso negato";
                }
@@ -36,12 +40,62 @@
                echo "Accesso negato";
            }
        } else {
-           echo "Accesso negato";
+           echo "";
        }
    } else {
        echo "Accesso negato";
    }
+
+
+   //Snack 3
+  //Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato:
+  // DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
+
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+
    ?>
+   
+
    
    
 
@@ -70,20 +124,53 @@
     <h1>Verifica Accesso</h1>
     
     <form method="GET" action="">
-        <label for="name">Nome:</label>
-             <input type="text" name="name" id="name" >
+        <label for="nome">Nome:</label>
+             <input type="text" name="nome" id="name" >
 
         <label for="mail">Email:</label>
-            <input type="email" name="mail" id="mail" >
+            <input type="email" name="e-mail" id="mail" >
 
         <label for="age">Età:</label>
-            <input type="number" name="age" id="age" >
+            <input type="number" name="età" id="age" >
 
         <input type="submit" value="Verifica Accesso">
     </form>
         
     
+        <?php  if (isset($_GET['nome']) && isset($_GET['e-mail']) && isset($_GET['età'])) { ?>
+        <div>   
+            <?php if($name && $mail && $age) {
+                echo 'Accesso riuscito';
+            } 
+            else { 
+                echo 'Accesso negato';
+            }
+            ?>
+        </div>
+    <?php }?>
     
+    <!-- 3 -->
+
+    <h1> Array di array</h1>
+
+            <div>
+
+                <ul>
+                    <?php foreach($posts as $key=>$post) { ?>
+                        <li>
+                            <?php echo $key; ?>
+
+                        <ul>
+                            <?php foreach($post as $item) { ?>
+
+                           <li><?php echo $item['title']." - ".$item['author']. " - ".$item 
+                            ['text']; ?></li>
+                        </li>
+                        <?php }?>
+                </ul>       
+            </li>
+            <?php } ?>
+                 
 
 
     
